93c47d389a757435d8d7f88330020cd31c81016b
Hjl1436 <hyeogjui568@gmail.com>
2025-07-19 06:49:13 +0900

    구조 수정 및 루터추가2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import Header from './components/Header/Header.js';
+import LoginPage from './Pages/LoginPage.js';
+import FirstPage from './Pages/FirstPage.js';
+
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Header/>
       <Routes>
-        <Route path="/" element={<MainPage />} />         {/* 첫화면 */}
+        <Route path="/" element={<FirstPage />} />         {/* 첫화면 */}
         <Route path="/login" element={<LoginPage />} />   {/* 로그인 */}
       </Routes>
-      <Footer />
     </Router>
   );
 }
