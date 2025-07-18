8478bf4a07eadef2147879975cbbd74b64688c6c
Hjl1436 <hyeogjui568@gmail.com>
2025-07-19 06:30:42 +0900

    루터 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header.js';
-import MainSection from './components/MainSection/MainSection';
-import Footer from './components/Footer/Footer';
-import FixedButton from './components/FixedButton/FixedButton';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
-    <div className="app-container">
-      <Header/>
-      <MainSection/>
-      <FixedButton/>
-      <Footer/>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />         {/* 첫화면 */}
+        <Route path="/login" element={<LoginPage />} />   {/* 로그인 */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
