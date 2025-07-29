fadbe15f5236153113f4c0abd608a846dd51fbdb
seojeong <sjeong11ee@gmail.com>
2025-07-29 14:10:10 +0900

    CreatePage: 과목 문자열화 및 기후변화융합학부/컴퓨터공학부 과목 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { auth } from './firebase';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { auth, db } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 
 import Header from './components/Header/Header.js';
@@ -23,6 +23,13 @@ function App() {
       <Routes>
         <Route path="/" element={<FirstPage user={user} />} />         {/* 첫화면 */}
         <Route path="/login" element={<LoginPage />} />   {/* 로그인 */}
+        {/* 관리자 */}
+        <Route path="/admin" 
+          element={
+            user && role === 'admin' ? <AdminPage /> : <Navigate to="/login" />   
+          }
+        />
+        <Route path="/new" element={<CreatePage />} />    {/* 자료업로드*/}
       </Routes>
     </Router>
   );
